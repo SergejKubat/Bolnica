8272809f2c459e121d3fa402c6e8fc64481f79ab
Sergej <sergej.kubat18@gmail.com>
2020-05-02 21:00:00 +0200

    - dodela klase za bazu

---
+++ src/com/bolnica/database/DatabaseHelper.java
@@ -0,0 +1,11 @@
+package com.bolnica.database;
+
+public class DatabaseHelper {
+    
+    private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String JDBC_DB_URL = "jdbc:mysql://localhost:3306/bolnica";
+    
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "";
+    
+}
